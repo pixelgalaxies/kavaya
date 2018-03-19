fd491eb18b7bed04bdfaf37ffd403c61895be81e
Dustin C <pixel.galaxies@gmail.com>
2018-03-19 11:38:44 -1000

    tweaks4

--- kavaya.js
+++ kavaya.js
@@ -54,3 +54,17 @@ function menuClose() {
 	
 }
 
+//Function to slide out side navigation
+var toggle = false;
+function slide() {
+	if (!toggle) {
+		document.querySelector(".slide-out").style.left="0px"
+		toggle = true;
+	}
+	else if (toggle) {
+		document.querySelector(".slide-out").style.left="-280px"
+		toggle = false;
+	}
+}
+
+
